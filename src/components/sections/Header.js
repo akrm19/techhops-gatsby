7be5cc810619cf52f8b13e01b90640a35d99b751
akrm19 <akrm19@gmail.com>
2019-12-27 01:34:59 -0600

    Remove unused part of Header

--- src/components/sections/Header.js
+++ src/components/sections/Header.js
@@ -1,77 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import { Container } from '@components/global';
 import ExternalLink from '@common/ExternalLink';
 import AustinImg from '../../images/austin.jpg';
 
-const Header = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        art_build: file(
-          sourceInstanceName: { eq: "art" }
-          name: { eq: "build" }
-        ) {
-          childImageSharp {
-            fluid(maxWidth: 1400) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <HeaderWrapper>
-        <Container>
-          <Grid>
-            {/* <Art>
-              <Img fluid={data.art_build.childImageSharp.fluid} />
-            </Art> */}
-            <Text>
-              <h1>
-                Developer
-                <br />
-                Traveler
-                <br />
-                Beer Lover
-              </h1>
+const Header = () => {
+  return (
+    <HeaderWrapper>
+      <Container>
+        <Grid>
+          <Text>
+            <h1>
+              Developer
               <br />
-              <p>
-                <StyledExternalLink href="https://github.com/akrm19">
-                  Check out my GitHub &nbsp;&#x2794;
-                </StyledExternalLink>
-              </p>
-            </Text>
-          </Grid>
-        </Container>
-      </HeaderWrapper>
-    )}
-  />
-);
+              Traveler
+              <br />
+              Beer Lover
+            </h1>
+            <br />
+            <p>
+              <StyledExternalLink href="https://github.com/akrm19">
+                Check out my GitHub &nbsp;&#x2794;
+              </StyledExternalLink>
+            </p>
+          </Text>
+        </Grid>
+      </Container>
+    </HeaderWrapper>
+  )
+}
 
 const HeaderWrapper = styled.header`
   background-image: url('${AustinImg}');
-  // background-color: ${props => props.theme.color.primary};
   padding-top: 96px;
 
   @media (max-width: ${props => props.theme.screen.md}) {
     padding-top: 128px;
   }
 `;
-
-// const Art = styled.figure`
-//   width: 100%;
-//   margin: 0;
-
-//   > div {
-//     @media (max-width: ${props => props.theme.screen.md}) {
-//       width: 100%;
-//     }
-//   }
-// `;
 
 const Grid = styled.div`
   display: grid;
@@ -82,8 +49,6 @@ const Grid = styled.div`
   @media (max-width: ${props => props.theme.screen.md}) {
     grid-template-columns: 1fr;
     grid-gap: 80px;
-
-
   }
 `;
 
