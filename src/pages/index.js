82dc92d4b3758b6ef9ffe47f53af4a29a853ac68
akrm19 <akrm19@gmail.com>
2019-12-28 17:55:56 -0600

    Rename brands.js to Tech.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ import Navbar from '@common/Navbar';
 
 import Header from '@sections/Header';
 import About from '@sections/About';
-import Brands from '@sections/Brands';
+import Brands from '@sections/Tech';
 import Projects from '@sections/Projects';
 import Faq from '@sections/Faq';
 import Footer from '@sections/Footer';
