0845b5b316e29ced9193a9063916c86c361435fa
akrm19 <akrm19@gmail.com>
2019-12-28 16:24:43 -0600

    Add project images

--- gatsby-config.js
+++ gatsby-config.js
@@ -35,6 +35,13 @@ module.exports = {
         path: `${__dirname}/src/images/photos`,
       },
     },
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `projects`,
+        path: `${__dirname}/src/images/projects`,
+      },
+    },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
