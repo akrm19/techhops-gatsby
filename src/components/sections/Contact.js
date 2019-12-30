4b264421474b0e85ae343c1dbc8d178e9b9a0e0a
akrm19 <akrm19@gmail.com>
2019-12-30 13:47:03 -0600

    Update Contact -add new photo -Add LinkedIn  and Github -Update styling

--- src/components/sections/Contact.js
+++ src/components/sections/Contact.js
@@ -1,12 +1,40 @@
 import React from 'react';
 import styled from 'styled-components';
+import { StaticQuery, graphql } from 'gatsby';
 import Img from 'gatsby-image';
+import ExternalLink from '@common/ExternalLink';
 import { Section, Container } from '@components/global';
 
+import GithubIcon from '@static/icons/github.svg';
+import LinkedInIcon from '@static/icons/linkedin.svg';
+import EmailIcon from '@static/icons/email.svg';
+
+
+const title = 'Contact';
+const contactText = 
+`Want to work together, offer me a high-paying job?`;
+
+const contactMethods = [
+  {
+    name: 'ron@techhops.com',
+    url: '',
+    icon: EmailIcon
+  },
+  {
+    name: 'LinkedIn',
+    url: 'https://www.linkedin.com/in/ronmacedo/',
+    icon: LinkedInIcon
+  },
+  {
+    name: 'GitHub',
+    url: 'https://github.com/akrm19',
+    icon: GithubIcon
+  },
+]
 
 const Grid = styled.div`
   display: grid;
-  grid-template-columns: 3fr 2fr;
+  grid-template-columns: 2fr 3fr;
   grid-gap: 40px;
   text-align: right;
   align-items: center;
@@ -44,24 +72,80 @@ const Grid = styled.div`
   }
 `;
 
-const Contact = () => {
-  return ( 
-    <Section id="contact">
-      <Container>
-        <Grid>
-          <div>
-            <h2>Contact</h2>
-            <p>
-              Test
-            </p>
-          </div>
-          {/* <Art>
-            <Img fluid={data.photo_ron.childImageSharp.fluid} />
-          </Art> */}
-        </Grid>
-      </Container>
-    </Section>
-  )
-}
- 
+const ContactMethods = styled.div`
+  padding-top: 5em;
+
+  a {
+    text-decoration: none;
+  }
+
+  img{
+    margin-left: .4em;
+  }
+
+  p {
+    
+  }
+`;
+
+const Contact = () => (
+  <StaticQuery
+    query={graphql`
+      query {
+        photo_ron: file(
+          sourceInstanceName: { eq: "photos" }
+          name: { eq: "austin_black_white" }
+        ) {
+          childImageSharp {
+            fluid(maxWidth: 860) {
+              ...GatsbyImageSharpFluid_withWebp_tracedSVG
+            }
+          }
+        }
+      }
+    `}
+    render={data => (
+      <Section id="contact">
+        <Container>
+          <Grid>
+            <div>
+              <h2>{title}</h2>
+              <p>
+                {contactText}
+              </p>
+              <ContactMethods>
+                {contactMethods.map(({name, url, icon}) => {
+                  return url 
+                  ?
+                    <ExternalLink key={name} href={url}>
+                      <p key={name}>
+                        {name}
+                        <img src={icon} alt="link" />
+                      </p>
+                    </ExternalLink>
+                  :
+                    <p key={name}>
+                        {name}
+                        <img src={icon} alt="link" />
+                    </p>
+                  })
+                }
+              </ContactMethods>
+            </div>
+            <Art>
+              <Img fluid={data.photo_ron.childImageSharp.fluid} />
+            </Art>
+          </Grid>
+        </Container>
+      </Section>
+    )}
+  />
+);
+
+const Art = styled.figure`
+  margin: 0;
+  // max-width: 480px;
+  width: 100%;
+`;
+
 export default Contact;
