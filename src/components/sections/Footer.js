1c2a23efc13c296108d816cb8ae9c50a900bfc61
akrm19 <akrm19@gmail.com>
2019-12-29 21:19:47 -0600

    Update Footer

--- src/components/sections/Footer.js
+++ src/components/sections/Footer.js
@@ -1,93 +1,70 @@
 import React from 'react';
 import styled from 'styled-components';
-import { StaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
 
 import { Container } from '@components/global';
 import ExternalLink from '@common/ExternalLink';
 
-import GithubIcon from '@static/icons/github.svg';
-import InstagramIcon from '@static/icons/instagram.svg';
-import TwitterIcon from '@static/icons/twitter.svg';
+// import GithubIcon from '@static/icons/github.svg';
+// import InstagramIcon from '@static/icons/instagram.svg';
+// import TwitterIcon from '@static/icons/twitter.svg';
 
-const SOCIAL = [
-  {
-    icon: GithubIcon,
-    link: 'https://github.com/ajayns/gatsby-absurd',
-  },
-  {
-    icon: InstagramIcon,
-    link: 'https://instagram.com/ajay_ns',
-  },
-  {
-    icon: TwitterIcon,
-    link: 'https://twitter.com/ajayns08',
-  },
-];
+// const SOCIAL = [
+//   {
+//     icon: GithubIcon,
+//     link: 'https://github.com/ajayns/gatsby-absurd',
+//   },
+//   {
+//     icon: InstagramIcon,
+//     link: 'https://instagram.com/ajay_ns',
+//   },
+//   {
+//     icon: TwitterIcon,
+//     link: 'https://twitter.com/ajayns08',
+//   },
+// ];
 
-const Footer = () => (
-  <StaticQuery
-    query={graphql`
-      query {
-        art_pot: file(
-          sourceInstanceName: { eq: "art" }
-          name: { eq: "customers_pot" }
-        ) {
-          childImageSharp {
-            fluid(maxWidth: 960) {
-              ...GatsbyImageSharpFluid_withWebp_tracedSVG
-            }
-          }
-        }
-      }
-    `}
-    render={data => (
-      <React.Fragment>
-        <Art>
-          <Img
-            fluid={data.art_pot.childImageSharp.fluid}
-            style={{ width: 480, maxWidth: '100%', marginBottom: -16 }}
-          />
-        </Art>
-        <FooterWrapper>
-          <StyledContainer>
-            <Copyright>
-              <h2>Absurd</h2>
-              <span>
-                Illustrations by
-                {` `}
-                <ExternalLink href="https://twitter.com/diana_valeanu">
-                  @diana_valeanu
-                </ExternalLink>
-              </span>
-            </Copyright>
-            <SocialIcons>
-              {SOCIAL.map(({ icon, link }) => (
-                <ExternalLink key={link} href={link}>
-                  <img src={icon} alt="link" />
-                </ExternalLink>
-              ))}
-            </SocialIcons>
-          </StyledContainer>
-        </FooterWrapper>
-      </React.Fragment>
-    )}
-  />
-);
+const Footer = () => {
+  return (
+    <React.Fragment>
+      <FooterWrapper>
+        <StyledContainer>
+          <Copyright>
+            <span>
+              Pictures by Some name
+            </span>
+            <span>
+              Design modified from Absurd
+            </span>
+            <span>
+              &copy; Copyright 2020, TechHops.
+            </span>
+          </Copyright>
+          {/* <SocialIcons>
+            {SOCIAL.map(({ icon, link }) => (
+              <ExternalLink key={link} href={link}>
+                <img src={icon} alt="link" />
+              </ExternalLink>
+            ))}
+          </SocialIcons> */}
+        </StyledContainer>
+      </FooterWrapper>
+    </React.Fragment>
+  )
+}
 
-const SocialIcons = styled.div`
-  display: flex;
+// const SocialIcons = styled.div`
+//   display: flex;
 
-  img {
-    margin: 0 8px;
-    width: 24px;
-    height: 24px;
-  }
+//   img {
+//     margin: 0 8px;
+//     width: 24px;
+//     height: 24px;
+//   }
 
-  @media (max-width: ${props => props.theme.screen.sm}) {
-    margin-top: 40px;
-  }
-`;
+//   @media (max-width: ${props => props.theme.screen.sm}) {
+//     margin-top: 40px;
+//   }
+// `;
 
 const FooterWrapper = styled.footer`
   background-color: ${props => props.theme.color.primary};
@@ -95,31 +72,32 @@ const FooterWrapper = styled.footer`
 `;
 
 const Copyright = styled.div`
-  font-family: ${props => props.theme.font.secondary};
-  ${props => props.theme.font_size.small};
-  color: ${props => props.theme.color.black.regular};
-
-  a {
-    text-decoration: none;
-    color: inherit;
-  }
-`;
-
-const Art = styled.figure`
+  width: 100%;
   display: flex;
   justify-content: center;
-  margin: 0;
-  margin-top: 48px;
-`;
+  text-align: center;
+  font-family: ${props => props.theme.font.secondary};
+  ${props => props.theme.font_size.xsmall};
+  color: ${props => props.theme.color.black.regular};
 
-const StyledContainer = styled(Container)`
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
+  span {
+    padding-left: .3em;
+    padding-right: .3em;
+  }
 
   @media (max-width: ${props => props.theme.screen.sm}) {
     flex-direction: column;
-    text-align: center;
+  }
+`;
+
+const StyledContainer = styled(Container)`
+  // display: flex;
+  // justify-content: space-between;
+  // align-items: center;
+
+  @media (max-width: ${props => props.theme.screen.sm}) {
+    // flex-direction: column;
+    // text-align: center;
   }
 `;
 
