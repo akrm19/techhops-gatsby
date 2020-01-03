9aa25d1104309b95e1921d043af7ba163332a1cc
akrm19 <akrm19@gmail.com>
2020-01-02 22:31:10 -0600

    Update favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -38,7 +38,7 @@ module.exports = {
         background_color: `#8bd8ed`,
         theme_color: `#8bd8ed`,
         display: `minimal-ui`,
-        icon: `static/favicon.svg`,
+        icon: `static/favicon-32x32.png`,
       },
     },
     {
